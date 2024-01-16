0c39aea3371f347b46a84493395d747f100121e3
Максим <mtolpekin@mail.ru>
2024-01-16 16:47:40 +0200

    add links for books

--- src/main/java/com/library/library/View/LibraryView.java
+++ src/main/java/com/library/library/View/LibraryView.java
@@ -3,8 +3,13 @@ package com.library.library.View;
 import com.library.library.Books.Book;
 import com.library.library.Repository.BookRepository;
 import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -20,29 +25,35 @@ public class LibraryView extends VerticalLayout {
         int index = 0;
         while (index < books.size()) {
             HorizontalLayout horizontalLayout = new HorizontalLayout();
-            horizontalLayout.getStyle().set("margin-right", "100px");
+            horizontalLayout.setAlignItems(FlexComponent.Alignment.CENTER);
 
             // Добавляем 4 книги в текущую группу
             for (int i = 0; i < 4 && index < books.size(); i++) {
                 Book book = books.get(index);
-                String bookHtml = "<div><div>" + book.getName() + "</div><div>" + book.getAuthor() + "</div></div>";
-                Html bookLabel = new Html(bookHtml);
-                horizontalLayout.add(bookLabel);
+                Anchor bookLink = new Anchor("https://localhost/book/" + book.getName() + "/" + book.getId());
+                Div bookInfo = new Div();
+                Text bookName = new Text(book.getName() + " ");
+                Text authorName = new Text(book.getAuthor());
+                Html lineBreak = new Html("<br>");
+
+                bookInfo.add(bookName, lineBreak, authorName);
+                bookLink.add(bookInfo);
+                bookLink.getElement().getStyle().set("text-decoration", "none");
+                bookLink.getElement().getStyle().set("color", "black");
+
+//                Element anchorElement = bookLink.getElement();
+//
+//                anchorElement.addEventListener("click", event -> {
+//                    getUI().ifPresent(ui -> ui.navigate("/book/"+ book.getName() + "/" + book.getId()));
+//                });
+                horizontalLayout.add(bookLink);
+
+
                 index++;
             }
 
             add(horizontalLayout);
         }
-
-//
-//        HorizontalLayout layout = new HorizontalLayout();
-//        layout.setPadding(true);
-//
-//        layout.setAlignItems(FlexComponent.Alignment.CENTER);
-//        layout.add(new TextArea("Text area 1"));
-//        layout.add(new TextArea("Text area 2"));
-//        layout.add(new TextArea("Text area 3"));
-//
     }
 //    private void showBooks() {
 //        if (MainView.searchField.isEmpty()) {
